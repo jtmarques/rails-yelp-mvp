c8bf227b5b385f3c628a17597151457929939837
jorge marques <jorgetmarques@gmail.com>
2023-08-16 20:17:49 +0100

    yelp. finished the main coding. not refactored. did not work on css

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -7,13 +7,15 @@ class RestaurantsController < ApplicationController
     @restaurant = Restaurant.find(params[:id])
   end
 
-  # def new
-  #   @restaurant = Restaurant.new
-  # end
+  def new
+    @restaurant = Restaurant.new
+  end
 
-  # def create
-  #   @restaurant = Restaurant.new(restaurant_params)
-  # end
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save
+    redirect_to restaurants_path()
+  end
 
   private
 
