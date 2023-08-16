eb5ada65d5390fa025d302fcef8b58ebe2f8e487
jorge marques <jorgetmarques@gmail.com>
2023-08-16 13:18:33 +0100

    seeding done

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
 
-  validates :rating, presence:, numericality: { only_integer: true }, inclusion: { in: [0..5] }
+  validates :rating, presence: true, numericality: { only_integer: true }, inclusion: { in: (0..5).to_a }
   validates :content, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+terrarea = Restaurant.create(name: "terrarea", address:"matosinhos", phone_number:"11", category:"belgian")
+santiago = Restaurant.create(name: "santiago", address:"porto", phone_number:"22", category:"chinese")
+tacobell = Restaurant.create(name: "taco bell", address:"senhora da hora", phone_number:"33", category:"french")
+sushiaria = Restaurant.create(name: "sushiaria", address:"leça da palmeira", phone_number:"44", category:"japanese")
+saomartino = Restaurant.create(name: "são martino", address:"matosinhos", phone_number:"55", category:"italian")
+
+rev1 = Review.create(rating:5, content:"love it!")
+rev2 = Review.create(rating:1, content:"hate it!")
+
+rev1.restaurant = terrarea
+rev2.restaurant = santiago
+
+rev1.save
+rev2.save
