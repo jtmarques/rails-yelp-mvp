01795283c71cce75c9b89a8e26d5f9898a3530f1
jorge marques <jorgetmarques@gmail.com>
2023-08-16 19:49:48 +0100

    yelp. fixed the review/new page mess

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,11 +14,24 @@ tacobell = Restaurant.create(name: "taco bell", address:"senhora da hora", phone
 sushiaria = Restaurant.create(name: "sushiaria", address:"leça da palmeira", phone_number:"44", category:"japanese")
 saomartino = Restaurant.create(name: "são martino", address:"matosinhos", phone_number:"55", category:"italian")
 
-rev1 = Review.create(rating:5, content:"love it!")
-rev2 = Review.create(rating:1, content:"hate it!")
+rev1 = Review.create(rating:1, content:"love it!")
+rev2 = Review.create(rating:2, content:"hate it!")
+rev3 = Review.create(rating:3, content:"love it!")
+rev4 = Review.create(rating:4, content:"hate it!")
+rev5 = Review.create(rating:5, content:"love it!")
+rev6 = Review.create(rating:1, content:"hate it!")
 
 rev1.restaurant = terrarea
 rev2.restaurant = santiago
+rev3.restaurant = tacobell
+rev4.restaurant = saomartino
+rev5.restaurant = terrarea
+rev6.restaurant = sushiaria
 
 rev1.save
 rev2.save
+rev3.save
+rev4.save
+rev5.save
+
+# too tired for iterations
